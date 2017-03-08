2db1029ff4f96bac7ddbe750faf0144125f338f0
scw1109 <scw1109@gmail.com>
2017-03-08 17:17:28 +0800

    Fix port.

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: target/universal/stage/bin/servant
+web: target/universal/stage/bin/servant $PORT
--- src/main/scala/com/github/scw1109/servant/Summon.scala
+++ src/main/scala/com/github/scw1109/servant/Summon.scala
@@ -6,6 +6,10 @@ import spark.Spark._
   * @author scw1109
   */
 object Summon extends App {
-  port(7777)
+  port(args.length match {
+    case 1 => args(0).toInt
+    case _ => 7777
+  })
+
   get("/", (_, _) => "Hello World !!")
 }
