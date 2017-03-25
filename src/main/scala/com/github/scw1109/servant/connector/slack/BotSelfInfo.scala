38572428d7eb96333e2f640fcaa9c3330faf7bb8
scw1109 <scw1109@gmail.com>
2017-03-25 10:54:26 +0800

    Cleanup old connector and command.

--- src/main/scala/com/github/scw1109/servant/connector/slack/BotSelfInfo.scala
+++
@@ -1,7 +0,0 @@
-package com.github.scw1109.servant.connector.slack
-
-/**
-  * @author scw1109
-  */
-case class BotSelfInfo(user: String, user_id: String,
-                       team: String, team_id: String, url: String)
--- src/main/scala/com/github/scw1109/servant/connector/slack/rtm/RtmResponse.scala
+++
@@ -1,6 +0,0 @@
-package com.github.scw1109.servant.connector.slack.rtm
-
-/**
-  * @author scw1109
-  */
-case class RtmResponse(url: String)
